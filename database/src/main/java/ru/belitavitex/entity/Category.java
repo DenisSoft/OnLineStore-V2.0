7e159655a49a6a4a0b2da1c1a1b86ec267e0a1dd
DenisSoft <bdv@bk.ru>
2017-05-26 14:35:58 +0300

    I replaced the methods in the class Category with annotations

--- database/src/main/java/ru/belitavitex/entity/Category.java
+++ database/src/main/java/ru/belitavitex/entity/Category.java
@@ -1,30 +1,22 @@
 package ru.belitavitex.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 /**
  * Created by Dzianis on 07.04.2017.
  */
+@Entity
+@Table(name = "Categories")
+@ToString
+@NoArgsConstructor
 public class Category extends BaseEntity {
+
+    @Getter
+    @Setter
     private String categoryName;
-
-    public Category(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Category(long id, String categoryName) {
-        super(id);
-        this.categoryName = categoryName;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    @Override
-    public String toString() {
-        return categoryName;
-    }
 }
