ffdc10f8ddcd41deddc90ee81466ca8e2ff20663
DenisSoft <bdv@bk.ru>
2017-05-26 14:29:22 +0300

    I replaced the methods in the class BaseEntity with annotations

--- database/src/main/java/ru/belitavitex/entity/BaseEntity.java
+++ database/src/main/java/ru/belitavitex/entity/BaseEntity.java
@@ -1,38 +1,20 @@
 package ru.belitavitex.entity;
+import lombok.*;
+import javax.persistence.*;
 
 /**
  * Created by Dzianis on 22.05.2017.
  */
+@Entity
+@Table(name = "id")
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
 public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private long id;
-
-    public BaseEntity() {
-    }
-
-    public BaseEntity(long id) {
-        this.id = id;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof BaseEntity)) return false;
-
-        BaseEntity that = (BaseEntity) o;
-
-        return getId() == that.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (getId() ^ (getId() >>> 32));
-    }
 }
